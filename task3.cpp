27c537a20874ff48f7b72492ce5f86de8c706660
bos <dbrechkawork@yandex.ru>
2022-03-03 11:30:45 +0600

    task add

--- task3.cpp
+++ task3.cpp
@@ -10,3 +10,11 @@ int main(int argc, char* argv[])
   // argv[1] is a step for parent process in "./task2", argv[2] is a step for child process in
   execl("./task2", "./task2", argv[1], argv[2], NULL);
 }
+
+/*
+TASK
+
+Напишите программу, вызывающую программу из задания 2 при
+помощи системного вызова exec(). Перед вызовом функции exec()
+предусмотрите вывод PID и PPID текущего процесса.
+*/
